3064fef884329f52172f4f9508bc2c6c85edd19b
Leonardo <leonardodesantana05@gmail.com>
2023-04-19 15:48:37 -0300

    correção da função encrypt quando não há vogais no texto

--- public/js/modules/encrypt.js
+++ public/js/modules/encrypt.js
@@ -10,12 +10,16 @@ export default function encryptText(){
     const textValue = textArea.value;
     
     if(textValue != ""){
-      const textEncrypt = textValue.replace(/e/gi, 'enter').replace(/i/gi,"imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat")
-      textResult.innerHTML = `<p  class="result">${textEncrypt}</p>`
-      copyArea.style.display = 'flex';
-      copyText(textEncrypt);
+      if(textValue.indexOf('e')!= -1 || textValue.indexOf('i')!= -1 || textValue.indexOf('a')!= -1 || textValue.indexOf('o')!= -1 || textValue.indexOf('u') != -1){
+        const textEncrypt = textValue.replace(/e/gi, 'enter').replace(/i/gi,"imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat")
+        textResult.innerHTML = `<p  class="result">${textEncrypt}</p>`
+        copyArea.style.display = 'flex';
+        copyText(textEncrypt);
+      }else{
+        textResult.innerHTML = "<h3>Parece que esse texto não contém palavras válidas para criptografar, certifique-se de ter vogais.</h3>" 
+      }
     }else{
-      textResult.innerHTML = "<h3>Você deve digitar um texto antes.</h3>" 
+        textResult.innerHTML = "<h3>Você deve digitar um texto antes.</h3>" 
     }
 
   })
